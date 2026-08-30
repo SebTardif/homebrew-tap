352d1a7fe554c01a2e92bde84458e0e3a8f94a74
Peter Steinberger <steipete@gmail.com>
2026-08-29 19:04:01 -0700

    chore: update octopool to 0.5.11

--- Formula/octopool.rb
+++ Formula/octopool.rb
@@ -1,26 +1,26 @@
 class Octopool < Formula
   desc "Org-authenticated GitHub read relay and gh-compatible cache shim"
   homepage "https://github.com/openclaw/octopool"
-  version "0.5.10"
+  version "0.5.11"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/openclaw/octopool/releases/download/v#{version}/octopool_#{version}_darwin_arm64.tar.gz"
-      sha256 "db691bf06fed777578ad4419e0d1a7bc70ffff1b946a552ba85533d390d63280"
+      sha256 "97f62511ffe38f48495a7345c1d8de63259f5534fed327e35fadfe5b34dd318f"
     else
       url "https://github.com/openclaw/octopool/releases/download/v#{version}/octopool_#{version}_darwin_amd64.tar.gz"
-      sha256 "508a3ec4d273074bfb301589886ccf7e7e153868d3926a6fac30d9da479fbb74"
+      sha256 "9aa429ac42ceafce7e3743d7c2eb98085db6d861456ee28d4d71e5deef432f4f"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/openclaw/octopool/releases/download/v#{version}/octopool_#{version}_linux_arm64.tar.gz"
-      sha256 "9ec3d4fa20a68f7be052c0711ed221434cfa6a375a35bece328ae87230e2661a"
+      sha256 "9584a4bf2006395fa866d396c29e038722d8bdeeb4e4411b2d992b229aa49c1b"
     else
       url "https://github.com/openclaw/octopool/releases/download/v#{version}/octopool_#{version}_linux_amd64.tar.gz"
-      sha256 "c54acc9c7bd60ee3e7188bc1756dfdfe5c5dcee7f74729ecd87928b345321340"
+      sha256 "04ef01cea390d51d5e7915a89a5efe04a7ebbc70daa560c35515632e9aeed8a5"
     end
   end
 
